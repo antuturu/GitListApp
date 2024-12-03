9c94efe7651d27e3f4feadb5673dd3c5bc87bb0e
Alex <antuturrru@gmail.com>
2024-12-03 16:28:54 +0300

    Initial Commit

---
+++ GitListApp/GitListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitListAppApp.swift
+//  GitListApp
+//
+//  Created by Александр Акимов on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
